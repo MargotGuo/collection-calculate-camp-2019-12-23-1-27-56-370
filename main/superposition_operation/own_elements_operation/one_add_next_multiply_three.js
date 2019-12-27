95691dace916fa8ce1f21021b25028df8132ab39
MargotGuo <gyqgdn@126.com>
2019-12-27 14:39:51 +0800

    [Guo Yuanqi]{feat}: folder [own_elements_operation] done

--- main/superposition_operation/own_elements_operation/one_add_next_multiply_three.js
+++ main/superposition_operation/own_elements_operation/one_add_next_multiply_three.js
@@ -1,4 +1,9 @@
 'use strict';
 function one_add_next_multiply_three(collection){
+  var result = collection.map(function(currentValue, currentIndex) {
+    return (currentValue + collection[currentIndex + 1]) * 3;
+  });
+  result.pop();
+  return result;
 }
 module.exports = one_add_next_multiply_three;
--- main/superposition_operation/own_elements_operation/rank_by_two_large_one_small.js
+++ main/superposition_operation/own_elements_operation/rank_by_two_large_one_small.js
@@ -1,5 +1,19 @@
 'use strict';
 function rank_by_two_large_one_small(collection){
-  //这里写代码。。。
+  var sortCollection = collection.sort(function (a, b) {
+    return a - b;
+  });
+  var result = sortCollection.map(function(currentValue, currentIndex) {
+    if (sortCollection[currentIndex + 2]) {
+      if (currentIndex % 3 !== 2) {
+        return sortCollection[currentIndex + 1];
+      } else {
+        return sortCollection[currentIndex - 2];
+      }
+    } else {
+      return currentValue;
+    }
+  });
+  return result;
 }
 module.exports = rank_by_two_large_one_small;
