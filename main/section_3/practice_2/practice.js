5ae497f574712b7c3a69dc677d1ee3c727616e61
MargotGuo <gyqgdn@126.com>
2019-12-27 10:42:53 +0800

    [Guo Yuanqi]{feat}: [Section3-practice2]done: create_updated_collection

--- main/section_3/practice_2/practice.js
+++ main/section_3/practice_2/practice.js
@@ -1,5 +1,12 @@
 function create_updated_collection(collection_a, object_b) {
-  //在这里写入代码
+  var outputCollection = collection_a.map(function(tempObj) {
+    if (object_b.value.indexOf(tempObj.key) !== -1) {
+      var reduction = Math.floor(tempObj.count / 3);
+      tempObj.count -= reduction;
+    }
+    return tempObj;
+  });
+  return outputCollection;
 }
 
 module.exports = create_updated_collection;
