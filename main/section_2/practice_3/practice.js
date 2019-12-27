733d9bd64a709ad30e00ce52470511dd72c65bd5
MargotGuo <gyqgdn@126.com>
2019-12-27 10:34:37 +0800

    [Guo Yuanqi]{feat}: [section2-pracitce3]done-count_same_elements

--- main/section_2/practice_3/practice.js
+++ main/section_2/practice_3/practice.js
@@ -1,5 +1,45 @@
 function count_same_elements(collection) {
-  //在这里写入代码
+  var formattedArr = standardizeArr(collection);
+  var keyArr = formattedArr.filter(getKeyArray);
+  var countArr = keyArr.map(getCountSummary);
+  return countArr;
+
+  function getCountSummary(element) {
+    var tempCount = formattedArr.reduce((accumulator, currentValue) => {
+      if (currentValue === element) {
+        accumulator++;
+        return accumulator;
+      }
+      return accumulator;
+    }, 0);
+
+    var tempObj = {
+      name: element,
+      summary: tempCount
+    }
+    return tempObj;
+  }
+}
+
+function getKeyArray(item, index, collection) {
+  return collection.indexOf(item) === index;
+}
+
+function standardizeArr(collection) {
+  var outputCollection = [];
+  for (var index = 0, len = collection.length; index < len; index++) {
+    if (collection[index].match(/\d+/)) {
+      var key = collection[index].charAt(0);
+      var value = collection[index].match(/\d+/);
+      while (value > 0) {
+        outputCollection.push(key);
+        value--;
+      }
+    } else {
+      outputCollection.push(collection[index]);
+    }
+  }
+  return outputCollection;
 }
 
 module.exports = count_same_elements;
